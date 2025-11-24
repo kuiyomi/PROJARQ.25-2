e929e3e0c1eea027d40141436c40b0b631bcdf4d
Matheus de Oliveira Ferreira <matheus.oferreira@hotmail.com>
2025-11-24 19:05:53 -0300

    ajustando erros dos modulos nao subirem

--- pedidos-service/src/main/resources/data.sql
+++ pedidos-service/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- Inserção dos clientes
 INSERT INTO clientes (cpf, nome, celular, endereco, email) VALUES ('9001', 'Huguinho Pato', '51985744566', 'Rua das Flores, 100', 'huguinho.pato@email.com');
 INSERT INTO clientes (cpf, nome, celular, endereco, email) VALUES ('9002', 'Luizinho Pato', '5199172079', 'Av. Central, 200', 'zezinho.pato@email.com');
@@ -75,3 +76,54 @@ INSERT INTO cardapio_produto (cardapio_id,produto_id) VALUES (1,3);
 INSERT INTO cardapio_produto (cardapio_id,produto_id) VALUES (2,1);
 INSERT INTO cardapio_produto (cardapio_id,produto_id) VALUES (2,3);
 >>>>>>> e9847fe (implementacao final (ou quase))
+=======
+insert into clientes (cpf, nome, celular, endereco, email)
+values ('123.456.789-00', 'Fulano', '51999999999', 'Rua X', 'cliente@teste.com');
+
+insert into ingredientes (id, descricao) values (1,'Disco de pizza');
+insert into ingredientes (id, descricao) values (2,'Porcao de tomate');
+insert into ingredientes (id, descricao) values (3,'Porcao de mussarela');
+insert into ingredientes (id, descricao) values (4,'Porcao de presunto');
+insert into ingredientes (id, descricao) values (5,'Porcao de calabresa');
+insert into ingredientes (id, descricao) values (6,'Molho de tomate');
+insert into ingredientes (id, descricao) values (7,'Porcao de azeitona');
+insert into ingredientes (id, descricao) values (8,'Porcao de oregano');
+insert into ingredientes (id, descricao) values (9,'Porcao de cebola');
+
+insert into produtos (id, descricao, preco) values (1,'Pizza Mussarela',45);
+insert into produtos (id, descricao, preco) values (2,'Pizza Calabresa',48);
+insert into produtos (id, descricao, preco) values (3,'Pizza Presunto',47);
+
+insert into receitas (id, titulo) values (1,'Receita Mussarela');
+insert into receitas (id, titulo) values (2,'Receita Calabresa');
+insert into receitas (id, titulo) values (3,'Receita Presunto');
+
+insert into produto_receita (produto_id, receita_id) values (1,1);
+insert into produto_receita (produto_id, receita_id) values (2,2);
+insert into produto_receita (produto_id, receita_id) values (3,3);
+
+-- Mussarela
+insert into receita_ingrediente values (1,1);
+insert into receita_ingrediente values (1,6);
+insert into receita_ingrediente values (1,3);
+insert into receita_ingrediente values (1,8);
+
+-- Calabresa
+insert into receita_ingrediente values (2,1);
+insert into receita_ingrediente values (2,6);
+insert into receita_ingrediente values (2,5);
+insert into receita_ingrediente values (2,9);
+insert into receita_ingrediente values (2,8);
+
+-- Presunto
+insert into receita_ingrediente values (3,1);
+insert into receita_ingrediente values (3,6);
+insert into receita_ingrediente values (3,4);
+insert into receita_ingrediente values (3,3);
+insert into receita_ingrediente values (3,8);
+
+insert into cardapios values (1,'Cardapio Principal');
+insert into cardapio_produto values (1,1);
+insert into cardapio_produto values (1,2);
+insert into cardapio_produto values (1,3);
+>>>>>>> d83a0c4 (ajustando erros dos modulos nao subirem)
