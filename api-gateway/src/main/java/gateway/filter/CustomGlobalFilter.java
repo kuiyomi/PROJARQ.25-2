b86ba10810268e53a1b5e07ea1431798a700eeac
PauloAugusto <paulomeissner7@gmail.com>
2025-11-24 20:15:55 -0300

    sipa foi

--- api-gateway/src/main/java/gateway/filter/CustomGlobalFilter.java
+++ api-gateway/src/main/java/gateway/filter/CustomGlobalFilter.java
@@ -1,4 +1,4 @@
-package main.java.gateway.filter;
+package gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
--- estoque-service/src/main/java/estoque/EstoqueServiceApplication.java
+++ estoque-service/src/main/java/estoque/EstoqueServiceApplication.java
@@ -2,14 +2,21 @@ package estoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(
-  scanBasePackages = { "estoque", "com.bcopstein.ex4_lancheriaddd_v1" }
-)
-@EnableDiscoveryClient
+@SpringBootApplication
+// FORÇA O SPRING A OLHAR PARA O PACOTE ANTIGO
+@EntityScan(basePackages = {
+    "estoque.dominio.entidades", // Se tiver locais
+    "com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades" // As entidades importadas
+})
+// (Opcional se os repositórios estiverem em estoque.*, mas bom garantir)
+@EnableJpaRepositories(basePackages = {
+    "estoque.adaptadores.repositorios"
+})
 public class EstoqueServiceApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(EstoqueServiceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(EstoqueServiceApplication.class, args);
+    }
 }
